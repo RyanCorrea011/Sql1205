4d1936106189f73ea51b7272f585c78615312721
RYAN CORREA <ryan.correa@fatec.sp.gov.br>
2023-05-12 21:32:59 -0300

    Cadastro SQL

--- WebApp_Noite/Contexto.cs
+++ WebApp_Noite/Contexto.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using WebApp_Noite.Tabelas;
 namespace WebApp_Noite
 {
     public class Contexto : DbContext
     {
         public Contexto(DbContextOptions<Contexto> opt)
             : base(opt) { }
+
+        public DbSet<Categorias> categorias { get; set; }
     } 
 }
